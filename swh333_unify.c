77a7bf6e0bce168f0f02238e8225325df627a4de
d2118370 <d2118370@gmail.com>
2016-02-07 19:49:11 -0500

    add

--- swh333_unify.c
+++ swh333_unify.c
@@ -1,3 +1,15 @@
+/*
+Your program will construct a type environment, and then perform unifications in that type environment, until
+either the program ends, or a unification fails.
+
+TYPE ::= TYPEVAR | PRIMITIVE_TYPE | FUNCTYPE | LISTTYPE;
+PRIMITIVE_TYPE ::= ‘int’ | ‘float’ | ‘long’ | ‘string’;
+TYPEVAR ::= ‘`’ VARNAME;  // Note, the character is a backwards apostrophe!
+VARNAME ::= [a-zA-Z][a-zA-Z0-9]*; // Initial letter, then can have numbers
+FUNCTYPE ::= ‘(‘ ARGLIST ‘)’ -> TYPE | ‘(‘ ‘)’ -> TYPE;
+ARGLIST ::=   TYPE ‘,’ ARGLIST | TYPE;
+LISTTYPE ::= ‘[‘ TYPE ‘]’;
+ */
 
 #include <string.h>
 #include <stdlib.h>
